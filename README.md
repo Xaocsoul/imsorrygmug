15e0d230b048c7822fe50fcae9df0698ba8eb025
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2026-07-07 12:10:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+[Mindustry-skull-button-main.zip](https://github.com/Xaocsoul/Mindustry-skull-button/files/11300162/Mindustry-skull-button-main.zip)
+# mindustry-skull-button
+Mindustry mod that adds a button when you click which a message with randomly color generated skull emoji icons(0.1% router)is sent to the chat
+original by vernaon
---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var rgbhex = "";
+var msg = "";
+const hexref = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"];
+
+Events.on(EventType.ClientLoadEvent, cons(load => {
+    Vars.ui.hudGroup.fill(cons(table => {
+        table.button(Icon.admin, Styles.cleari, run(() => {
+        
+        generate_and_send();
+        
+        })).width(50).height(50).name("admin button");
+        
+        table.top().marginLeft(900);
+    }));
+}));
+
+function generate_and_send() {
+    msg = "";
+    for(let i=0; i<Mathf.ceil(Mathf.random() * 16); i++) {
+       rgbhex = "[#";
+       for(let k=0; k<6; k++) {
+          rgbhex+=hexref[Mathf.floor(Mathf.random() * 16)];
+       };
+    let rand = Mathf.ceil(Mathf.random()*1000);
+    msg+=rgbhex + "]" + ((rand == 1) ? "": "☠️");
+    };
+    Call.sendChatMessage(msg);
+};
