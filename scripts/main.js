50b96788af6da389aaf7d5835e75fb53d1b36af1
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2026-07-08 01:47:02 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,18 @@ const hexref = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c",
 Events.on(EventType.ClientLoadEvent, cons(load => {
     Vars.ui.hudGroup.fill(cons(table => {
         table.button(Icon.admin, Styles.cleari, run(() => {
+
+            getvar();
+            
+        })).width(50).height(50).name("variable button");
+
+        table.top().marginLeft(950);
+    }
+}
+
+Events.on(EventType.ClientLoadEvent, cons(load => {
+    Vars.ui.hudGroup.fill(cons(table => {
+        table.button(Icon.admin, Styles.cleari, run(() => {
         
         generate_and_send();
         
@@ -14,6 +26,28 @@ Events.on(EventType.ClientLoadEvent, cons(load => {
     }));
 }));
 
+function getvar() {
+    const inputDialog = new BaseDialog("Enter Your String");
+    let myCustomString = "";
+    inputDialog.cont.add("Please type your text below:").row();
+    const field = inputDialog.cont.field("", text => {
+    }).get(); 
+
+    inputDialog.buttons.button("Confirm", () => {
+        myCustomString = field.getText();
+    
+        Vars.ui.hudfrag.showToast("Saved string: " + myCustomString);
+    
+        inputDialog.hide();
+    }).size(160, 50);
+
+    inputDialog.buttons.button("Cancel", () => {
+        inputDialog.hide();
+    }).size(160, 50);
+
+    inputDialog.show();
+}
+
 function generate_and_send() {
     msg = "";
     for(let i=0; i<1; i++) {
@@ -22,7 +56,7 @@ function generate_and_send() {
           rgbhex+=hexref[Mathf.floor(Mathf.random() * 16)];
        };
     let rand = Mathf.ceil(Mathf.random()*1000);
-    msg+=rgbhex + "]" + ((rand == 1) ? "<@1186833004899876874> GMug id be scared of beacon if i was you ngl": "<@1186833004899876874> *Pings gmug cutely*");
+    msg+=rgbhex + "]" + ((rand == 1) ? "<@1186833004899876874> GMug id be scared of beacon if i was you ngl": msgvar);
     };
     Call.sendChatMessage(msg);
 };
